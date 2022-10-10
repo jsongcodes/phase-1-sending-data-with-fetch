8008d643e05861415e3563f84a39c9797bcc6932
Jasmin <ssongnari@gmail.com>
2022-10-10 11:46:58 -0400

    solution

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+
+
+
+function getID(object) {
+    document.body.innerHTML = object.id
+}
+
+function errorFunc(error) {
+    document.body.innerHTML = error.message;
+}
+
+function submitData(userName, userEmail) {
+    const formData = {
+        name: `${userName}`,
+        email: `${userEmail}`
+    };
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            'Accept': "application/json",
+        },
+        body: JSON.stringify(formData)
+    };
+    return fetch("http://localhost:3000/users", configurationObject)
+        .then(res => res.json())
+        .then(getID)
+        .catch(errorFunc);
+
+}
